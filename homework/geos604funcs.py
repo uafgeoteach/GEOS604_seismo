d66b25dddd1799adbc1d875fb0828d5b892d57e1
bch0w <bryant.h.c@gmail.com>
2025-04-15 12:56:43 -0800

    uploading geos419 lab7 with minor mods to make it work here

--- homework/geos604funcs.py
+++ homework/geos604funcs.py
@@ -23,6 +23,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.animation as animation
 import mplstereonet  
+from obspy import UTCDateTime
 from obspy.imaging.beachball import aux_plane
 from numpy import e, pi, sin, cos
 plt.rcParams["animation.html"] = "jshtml"  # needed to show animations inline 
@@ -104,16 +105,17 @@ def plot_polarities(takeoff_angles=None, azimuths=None, polarities=None,
                 markersize=10)
 
     plt.show()
-    
 
-def plot_waveforms(st, st_2=None, fmt="relative"):
+def plot_waveforms(st, st_2=None, marks=None, fmt="absolute",
+                   tick_every=None):
     """
     Generic Obspy Stream waveform plotter for use in GEOS419 Time Series Lab
-    
     :type st: obspy.core.stream.Stream
     :param st: Stream object to plot
     :type st_2: obspy.core.stream.Stream
     :param st_2: Optional second stream to plot together with `st` on the same axis
+    :type marks: list of str
+    :param marks: put a vertical line at all UTC times listed
     :type fmt: str
     :param fmt: x-axis label formatter, 'absolute' for timestamps, 'relative' for starttime t=0
     """
@@ -121,12 +123,14 @@ def plot_waveforms(st, st_2=None, fmt="relative"):
     if len(st) == 1:
         axs = [axs]
     plt.subplots_adjust(hspace=0.025)
-    
+
     # X-axis formatting
     if fmt == "absolute":
         time_format = "matplotlib"
-        axs[-1].xaxis.set_major_formatter(mdates.DateFormatter("%H:%M:%S"))  # %H:%M:%S.%f
+        axs[-1].xaxis.set_major_formatter(mdates.DateFormatter("%H:%M:%S.%f"))  # %H:%M:%S.%f
         axs[-1].tick_params(axis="x", labelrotation=20)
+        if tick_every:
+            axs[-1].xaxis.set_major_locator(mdates.MinuteLocator(interval=tick_every))
     else:
         time_format = "relative"
 
@@ -134,35 +138,105 @@ def plot_waveforms(st, st_2=None, fmt="relative"):
     st_plot = st.copy()
     st_plot.sort()
     time = st_plot[0].times(time_format)  # assuming all have the same time axis
-    
+
     # Determine what to do if we plot two waveforms or just one
     if st_2 is None:
         st_2_plot = [None for _ in range(len(st))]
+        alpha = 1
     else:
         st_2_plot = st_2.copy()
         st_2_plot.sort()
         time_2 = st_2_plot[0].times(time_format)
-    
+        alpha = 0.8
+
     # Loop through each trace and plot
     for i, (tr, tr_2) in enumerate(zip(st_plot, st_2_plot)):
         axs[i].plot(time, tr.data, label=tr.get_id(), c="k", lw=0.75,
-                    zorder=10)
-        # Plot the second set of waveforms below and slightly transparent 
+                    zorder=10, alpha=alpha)
+        # Plot the second set of waveforms below and slightly transparent
         if tr_2:
             axs[i].plot(time_2, tr_2.data, label=tr_2.get_id(), c="r", lw=0.75,
                         zorder=9, alpha=0.75)
-            
+
         axs[i].legend(loc="upper right", frameon=False)
         axs[i].grid()
-        
+
+    if marks:
+        if fmt == "absolute":
+            for mark in marks:
+                m = UTCDateTime(mark).matplotlib_date
+                axs[0].axvline(m, c="r", alpha=0.5)
+        else:
+            for mark in marks:
+                m = UTCDateTime(mark) - time[0]  # relative time
+                axs[0].axvline(m, c="r", alpha=0.5)
+
     # General Plot Aesthetics
     axs[0].set_title(f"{st[0].stats.starttime} - {st[0].stats.endtime}")
     axs[-1].set_xlabel("Time [s]")
     axs[int((len(st)-1)/2)].set_ylabel("Amplitude")
     plt.xlim([time.min(), time.max()])
-    
-    plt.show()
 
+    plt.show()
+    
+
+# def plot_waveforms(st, st_2=None, fmt="relative"):
+#     """
+#     Generic Obspy Stream waveform plotter for use in GEOS419 Time Series Lab
+#     
+#     :type st: obspy.core.stream.Stream
+#     :param st: Stream object to plot
+#     :type st_2: obspy.core.stream.Stream
+#     :param st_2: Optional second stream to plot together with `st` on the same axis
+#     :type fmt: str
+#     :param fmt: x-axis label formatter, 'absolute' for timestamps, 'relative' for starttime t=0
+#     """
+#     f, axs = plt.subplots(len(st), figsize=(10, 8), sharex=True, sharey=True)
+#     if len(st) == 1:
+#         axs = [axs]
+#     plt.subplots_adjust(hspace=0.025)
+#     
+#     # X-axis formatting
+#     if fmt == "absolute":
+#         time_format = "matplotlib"
+#         axs[-1].xaxis.set_major_formatter(mdates.DateFormatter("%H:%M:%S"))  # %H:%M:%S.%f
+#         axs[-1].tick_params(axis="x", labelrotation=20)
+#     else:
+#         time_format = "relative"
+# 
+#     # Make copies so we can sort the waveforms in-place
+#     st_plot = st.copy()
+#     st_plot.sort()
+#     time = st_plot[0].times(time_format)  # assuming all have the same time axis
+#     
+#     # Determine what to do if we plot two waveforms or just one
+#     if st_2 is None:
+#         st_2_plot = [None for _ in range(len(st))]
+#     else:
+#         st_2_plot = st_2.copy()
+#         st_2_plot.sort()
+#         time_2 = st_2_plot[0].times(time_format)
+#     
+#     # Loop through each trace and plot
+#     for i, (tr, tr_2) in enumerate(zip(st_plot, st_2_plot)):
+#         axs[i].plot(time, tr.data, label=tr.get_id(), c="k", lw=0.75,
+#                     zorder=10)
+#         # Plot the second set of waveforms below and slightly transparent 
+#         if tr_2:
+#             axs[i].plot(time_2, tr_2.data, label=tr_2.get_id(), c="r", lw=0.75,
+#                         zorder=9, alpha=0.75)
+#             
+#         axs[i].legend(loc="upper right", frameon=False)
+#         axs[i].grid()
+#         
+#     # General Plot Aesthetics
+#     axs[0].set_title(f"{st[0].stats.starttime} - {st[0].stats.endtime}")
+#     axs[-1].set_xlabel("Time [s]")
+#     axs[int((len(st)-1)/2)].set_ylabel("Amplitude")
+#     plt.xlim([time.min(), time.max()])
+#     
+#     plt.show()
+# 
     
 def wave_eq_animation(length_of_string=1.5, duration_of_simulation=3,
                       junction=0.7, c_1=1.0, c_2=0.5, source_location=0,
---
+++ misc/geos604funcs.py
@@ -0,0 +1,1 @@
+../homework/geos604funcs.py
